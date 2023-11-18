a03ee21296ff08494493b178a9eddbe7a41ac781
Yorick Downe <yorickdowne@gmail.com>
2023-11-18 16:56:39 +0000

    pg_restore correct input file

--- load-snapshot.sh
+++ load-snapshot.sh
@@ -1,5 +1,6 @@
 #/usr/bin/env sh
+set -eu
 
 if [ -f /tmp/snapshot.pgdump ]; then # Not a directory and so was supplied by user
-  pg_restore -f /tmp/snapshot.pgdump # Rely on ENV to give us the right DB
+  pg_restore < /tmp/snapshot.pgdump # Rely on ENV to give us the right DB
 fi
